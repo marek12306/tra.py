6cfcfa1632d6ca427bdaaf0f5f676c7cc9a01735
deepivin <marek12306@gmail.com>
2021-01-18 21:19:30 +0100

    bot tra.py

---
+++ bot_tra.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import discord
+import requests
+import random
+import imghdr
+import io
+
+trapy = ['astolfo_(fate)','astolfo','','felix_argyle']
+
+class Trapy(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.content.strip() == 'tra.py':
+            r = requests.get('https://cure.ninja/booru/api/json?q={}+trap&f={}&o=r&s=1'.format(random.choice(trapy), 'e' if message.channel.is_nsfw() else 's'))
+            url = r.json()['results'][0]['url']
+            image = io.BytesIO(requests.get(url).content)
+            imgformat = imghdr.what(image)
+            await message.channel.send(file=discord.File(image, filename=f'trap.{imgformat}'))
+
+client = Trapy()
+
+client.run(open('.token', 'r').read())
