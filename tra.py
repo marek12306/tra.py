ab00fb18968f4ebf5ad8eb34d722b6293658aa77
deepivin <marek12306@gmail.com>
2021-10-02 19:03:42 +0200

    raw

--- tra.py
+++ tra.py
@@ -3,17 +3,21 @@ import requests
 import webbrowser
 import random
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--sech", help="trap nsfw", action="store_true")
 parser.add_argument("--url", help="daj urla", action="store_true")
+parser.add_argument("--raw", help="obrazek raw", action="store_true")
 args = parser.parse_args()
 
 trapy = ['astolfo_(fate)','astolfo','felix_argyle','tsukudani_norio','arikawa_hime']
 
 r = requests.get('https://cure.ninja/booru/api/json?q={}+trap&f={}&o=r&s=1'.format(random.choice(trapy), 'e' if args.sech else 's'))
 url = r.json()['results'][0]['page']
-if args.url:
+if args.raw:
+    sys.stdout.buffer.write(requests.get(r.json()['results'][0]['url']).content)
+elif args.url:
     print(url)
 else:
     webbrowser.open(url)
